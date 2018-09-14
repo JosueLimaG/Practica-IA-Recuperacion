37abc6e2de6a5d760256a0e17f25a83b45eaf137
JosueLimaG <josueeltipofeliz@gmail.com>
2018-09-14 12:04:13 -0400

    Proyecto

---
+++ Assets/Scipts/EnemyScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyScript : MonoBehaviour
+{
+    public Transform[] puntos;
+    private int destino = 0;
+    private NavMeshAgent agent;
+    Transform player;
+    RaycastHit hit;
+    Ray ray;
+    public float maxRange = 50;
+    bool seguirJugador = false;
+
+	void Start ()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+        SiguientePunto();
+	}
+
+    void SiguientePunto()
+    {
+        agent.destination = puntos[destino].position;
+        destino = (destino + 1) % puntos.Length;
+    }
+	
+	void Update ()
+    {
+        if(!agent.pathPending && agent.remainingDistance < 0.5f)
+        {
+            SiguientePunto();
+        }
+        if (Vector3.Distance(transform.position, player.position) < maxRange)
+        {
+            if (Physics.Raycast(transform.position, (player.position - transform.position), out hit, maxRange))
+            {
+                if (hit.transform == player)
+                {
+                    seguirJugador = true;
+                }
+            }
+        }
+
+        if (seguirJugador)
+        {
+            GetComponent<NavMeshAgent>().destination = player.position;
+        }
+    }
+}
---
+++ Assets/Scipts/InputScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InputScript : MonoBehaviour
+{
+    private Rigidbody rigid;
+    private float x, y, z;
+    public float sensibilidad = 25f;
+    public GameObject victoria;
+
+	void Start ()
+    {
+        rigid = GetComponent<Rigidbody>();
+	}
+	
+	void Update ()
+    {
+        x = Input.GetAxis("Mouse X");
+        z = Input.GetAxis("Mouse Y");
+        y = 0;
+        rigid.velocity = new Vector3(z * sensibilidad, y * sensibilidad, -x * sensibilidad);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag != "Final")
+        {
+            SceneManager.LoadScene(0);
+        }
+        else
+        {
+            victoria.SetActive(true);
+        }
+    }
+}
